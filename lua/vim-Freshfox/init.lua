0b0a1e2bf9ff4282890caf97eb13f53c74bcaa72
IJJA3141 <alex.estella.cj@gmail.com>
2023-07-15 14:07:41 +0200

    03.01 all functions

--- lua/vim-Freshfox/init.lua
+++ lua/vim-Freshfox/init.lua
@@ -2,7 +2,24 @@ local M = {}
 
 M.refresh = function()
   local path = vim.fn.fnamemodify('~', ':p')
-  os.execute('start '.. path .. '/AppData/Local/nvim-data/lazy/vim-Freshfox/vim-Freshfox.exe')
+  os.execute('start ' .. path .. '/AppData/Local/nvim-data/lazy/vim-Freshfox/vim-Freshfox.exe')
+  print('Firefox synced')
+end
+
+M.setup = function()
+  vim.api.nvim_create_autocmd('BufWritePost', {
+    group = vim.api.nvim_create_augroup('Freshfox', { clear = true }),
+    callback = M.refresh,
+   -- pattern = filetypes,
+  })
+end
+
+M.vimtex = function ()
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'VimtexEventCompileSuccess',
+    callback = M.refresh,
+    group = vim.api.nvim_create_augroup('vimtex_refresh_firefox', {clear = true})
+  })
 end
 
 return M
